fc2351746ca26d080a979f02d14118d91cc93f37
haohandaitruongphu <hiephomhinh55@gmail.com>
2024-03-06 16:28:37 +0700

    first

---
+++ group1/group1.js
@@ -0,0 +1,25 @@
+function submitQuiz() {
+    // let score = 0;
+  
+    // // Lặp qua từng câu hỏi
+    // for (let i = 1; i <= 10; i++) {
+    //   const answer = document.querySelector(`input[name="question${i}"]:checked`);
+  
+    //   // Kiểm tra nếu có câu trả lời được chọn
+    //   if (answer) {
+    //     const answerValue = answer.value;
+  
+    //     // Kiểm tra câu trả lời có đúng không
+    //     if (answerValue === "true") {
+    //       score++;
+    //     }
+    //   } else {
+    //     alert(`Bạn chưa chọn câu trả lời cho câu hỏi ${i}`);
+    //     return; // Dừng hàm nếu có bất kỳ câu hỏi nào chưa được trả lời
+    //   }
+    // }
+  
+    // // Hiển thị điểm số
+    // alert(`Bạn đã trả lời đúng ${score} câu trên tổng số 10 câu.`);
+    window.location.href = "../group2/group2.html";
+}
---
+++ group2/group2.js
@@ -0,0 +1,29 @@
+function submitQuiz() {
+    // let score = 0;
+  
+    // // Lặp qua từng câu hỏi
+    // for (let i = 1; i <= 10; i++) {
+    //   const selectedOption = document.querySelector(`input[name="question${i}"]:checked`);
+  
+    //   // Kiểm tra nếu có câu trả lời được chọn
+    //   if (selectedOption) {
+    //     const answerValue = selectedOption.value;
+  
+    //     // Kiểm tra câu trả lời có đúng không
+    //     // Ở đây, bạn cần so sánh giá trị của answerValue với đáp án đúng của từng câu hỏi
+    //     // Ví dụ: nếu đáp án đúng của câu hỏi 1 là "A", bạn có thể so sánh với answerValue === "A"
+    //     // và tăng điểm số nếu đáp án đúng
+    //     // Ví dụ:
+    //     // if (i === 1 && answerValue === "A") {
+    //     //     score++;
+    //     // }
+    //   } else {
+    //     alert(`Bạn chưa chọn câu trả lời cho câu hỏi ${i}`);
+    //     return; // Dừng hàm nếu có bất kỳ câu hỏi nào chưa được trả lời
+    //   }
+    // }
+  
+    // // Hiển thị điểm số
+    // alert(`Bạn đã trả lời đúng ${score} câu trên tổng số 10 câu.`);
+    window.location.href = "../group3/group3.html";
+  }
